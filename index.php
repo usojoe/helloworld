d9e654d05203c8f24d092e3e95691f37ec33c432
Joe Lee <josephandrewlee@g.harvard.edu>
2015-09-15 08:50:54 -0400

    First commit; added readme

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<!-- TemplateBeginEditable name="doctitle" -->
+<title>HELLO WORLD!!</title>
+<!-- TemplateEndEditable -->
+<!-- TemplateBeginEditable name="head" -->
+<!-- TemplateEndEditable -->
+</head>
+
+<body>
+
+<h1>HELLO WORLD!</h1>
+</body>
+</html>
